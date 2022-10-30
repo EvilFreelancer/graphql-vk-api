4c35c127451315b2b373c1a50e0e0c647a03b880
Paul Rock <paul@drteam.rocks>
2022-10-31 00:43:05 +0300

    To server added support of ENV variables

--- server.js
+++ server.js
@@ -1,11 +1,24 @@
-import express from 'express'
-import graphqlHTTP from "express-graphql"
-import vkGraphQl from './src/vk-graphql'
+import express from 'express';
+import graphqlHTTP from 'express-graphql';
+import vkGraphql from './src/vk-graphql';
 
+// Read env variables from file
+require('dotenv').config();
+const bind = process.env.APP_BIND || '0.0.0.0';
+const port = process.env.APP_PORT || 3001;
+const endpoint = process.env.APP_ENDPOINT || '/graphql';
+const token = process.env.VK_API_KEY;
+
+// Set token for work with API
+vkGraphql.setToken(token);
+
+// Init application
 const app = express();
 
-app.use('/', graphqlHTTP({ schema: vkGraphQl.Schema, graphiql: true }));
+// Set routes
+app.use(endpoint, graphqlHTTP({ schema: vkGraphql.Schema, graphiql: true }));
 
-app.listen(3000,()=>{
-    console.log("App started at port 3000");
+// Start application in foreground
+app.listen(port, bind, () => {
+  console.log(`App started at ${bind}:${port}${endpoint}`);
 });
